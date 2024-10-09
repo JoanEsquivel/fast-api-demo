934e2eb81758989d07bd611954c4d5842dfd3ce3
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-10-08 22:05:23 -0300

    feat: adding tags to sort my collection + endpoints

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Annotated
 
-app = FastAPI()
+app = FastAPI(title="Fast API demo for JoanMedia",   description="This is a custom description for my demo API.", version="1.0.0")
 
 # ----------------- Path Parameters -----------------
-@app.get("/users/me")
+@app.get("/users/me", tags=["Users"])
 async def read_user_me():
     return {"user_id": "the current user"}
 
 
-@app.get("/users/{user_id}")
+@app.get("/users/{user_id}", tags=["Users"])
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
@@ -24,7 +24,7 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
-@app.get("/models/{model_name}")
+@app.get("/models/{model_name}", tags=["Models"])
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
@@ -39,7 +39,7 @@ async def get_model(model_name: ModelName):
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/")
+@app.get("/items/", tags=["Items"])
 async def read_item(skip: int = 0, limit: int = 10):
     # Slicing is a way to extract a portion of a list, tuple, string, or any other sequence type. 
     # sequence[start:stop:step]
@@ -48,7 +48,7 @@ async def read_item(skip: int = 0, limit: int = 10):
     # step: The interval between each index (optional).
     return fake_items_db[skip : skip + limit]
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", tags=["Items"])
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id}
     if q:
@@ -59,7 +59,7 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
         )
     return item
 
-@app.get("/users/{user_id}/items/{item_id}")
+@app.get("/users/{user_id}/items/{item_id}", tags=["Items"])
 async def read_user_item(
     user_id: int, item_id: str, q: str | None = None, short: bool = False
 ):
@@ -79,11 +79,11 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
-@app.post("/items/")
+@app.post("/items/", tags=["Items"])
 async def create_item(item: Item):
     return item
 
-@app.put("/items/{item_id}")
+@app.put("/items/{item_id}", tags=["Items"])
 async def update_item(item_id: int, item: Item, q: Annotated[str | None, Query(title="Test", description="Query string for the items to search in the database that have a good match", min_length=3,max_length=50,pattern="^fixedquery$",
             deprecated=True,)] = None):
     result = {"item_id": item_id, **item.dict()}
