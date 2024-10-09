61beaeac0e08d203d46636397f3ac6c6a996b708
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-10-08 22:10:08 -0300

    fix: using summary instead of descriptions

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from typing import Annotated
 app = FastAPI(title="Fast API demo for JoanMedia",   description="This is a custom description for my demo API.", version="1.0.0")
 
 # ----------------- Path Parameters -----------------
-@app.get("/users/me", tags=["Users"], description="Get the current user")
+@app.get("/users/me", tags=["Users"], summary="Get the current user")
 async def read_user_me():
     return {"user_id": "the current user"}
 
 
-@app.get("/users/{user_id}", tags=["Users"], description="Get a specific user")
+@app.get("/users/{user_id}", tags=["Users"], summary="Get a specific user")
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
@@ -24,7 +24,7 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
-@app.get("/models/{model_name}", tags=["Models"], description="Get a specific LLM model")
+@app.get("/models/{model_name}", tags=["Models"], summary="Get a specific LLM model")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
@@ -39,7 +39,7 @@ async def get_model(model_name: ModelName):
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/", tags=["Items"], description="Get all items")
+@app.get("/items/", tags=["Items"], summary="Get all items")
 async def read_item(skip: int = 0, limit: int = 10):
     # Slicing is a way to extract a portion of a list, tuple, string, or any other sequence type. 
     # sequence[start:stop:step]
@@ -48,7 +48,7 @@ async def read_item(skip: int = 0, limit: int = 10):
     # step: The interval between each index (optional).
     return fake_items_db[skip : skip + limit]
 
-@app.get("/items/{item_id}", tags=["Items"], description="Get a specific item")
+@app.get("/items/{item_id}", tags=["Items"], summary="Get a specific item")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id}
     if q:
@@ -59,7 +59,7 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
         )
     return item
 
-@app.get("/users/{user_id}/items/{item_id}", tags=["Items"], description="Get a specific item for a user")
+@app.get("/users/{user_id}/items/{item_id}", tags=["Items"], summary="Get a specific item for a user")
 async def read_user_item(
     user_id: int, item_id: str, q: str | None = None, short: bool = False
 ):
@@ -79,11 +79,11 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
-@app.post("/items/", tags=["Items"], description="Create an item")
+@app.post("/items/", tags=["Items"], summary="Create an item")
 async def create_item(item: Item):
     return item
 
-@app.put("/items/{item_id}", tags=["Items"], description="Update an item")
+@app.put("/items/{item_id}", tags=["Items"], summary="Update an item")
 async def update_item(item_id: int, item: Item, q: Annotated[str | None, Query(title="Test", description="Query string for the items to search in the database that have a good match", min_length=3,max_length=50,pattern="^fixedquery$",
             deprecated=True,)] = None):
     result = {"item_id": item_id, **item.dict()}
