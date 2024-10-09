0dadac15e3364bf2e4981cca4f20075db5d3feb7
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-10-08 21:29:58 -0300

    feat: query parameter examples

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ from enum import Enum
 
 from fastapi import FastAPI
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
+# ----------------- Path Parameters -----------------
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
@@ -21,6 +15,14 @@ async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
+
+#Models with fixed data
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -30,3 +32,41 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+# ----------------- Query Parameters -----------------
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    # Slicing is a way to extract a portion of a list, tuple, string, or any other sequence type. 
+    # sequence[start:stop:step]
+    # start: The index where the slice starts (inclusive).
+    # stop: The index where the slice ends (exclusive).
+    # step: The interval between each index (optional).
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
