5e13a08b5068640f9c9ab3699c3e6fa9b6c48e88
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-10-10 22:31:04 -0300

    feat: OAuth2 with Password (and hashing), Bearer with JWT tokens

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from enum import Enum
-from fastapi import FastAPI, Query, Path, Body, status, Depends, HTTPException, status
-from pydantic import BaseModel, Field
-from typing import Annotated, Literal
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "a92e0e525caf0655d882d08d7a630da13851677b2228fce603c3b8dc9990b2ff"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
+    }
 }
 
-app = FastAPI()
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class User(BaseModel):
@@ -42,27 +46,64 @@ class UserInDB(User):
     hashed_password: str
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -75,129 +116,31 @@ async def get_current_active_user(
 
 
 @app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
-    return {"access_token": user.username, "token_type": "bearer"}
 
-
-@app.get("/users/me")
+@app.get("/users/me/", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return current_user
-# ----------------- Path Parameters -----------------
-# @app.get("/users/me", tags=["Users"], summary="Get the current user")
-# async def read_user_me():
-#     return {"user_id": "the current user"}
 
-
-# @app.get("/users/{user_id}", tags=["Users"], summary="Get a specific user")
-# async def read_user(user_id: Annotated[str, Path(title='User ID', description='The ID of the user to get', min_length=3, max_length=50)]):
-#     return {"user_id": user_id}
-
-
-
-# #Models with fixed data
-
-# class ModelName(str, Enum):
-#     alexnet = "alexnet"
-#     resnet = "resnet"
-#     lenet = "lenet"
-
-# @app.get("/models/{model_name}", tags=["Models"], summary="Get a specific LLM model")
-# async def get_model(model_name: ModelName):
-#     if model_name is ModelName.alexnet:
-#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-#     if model_name.value == "lenet":
-#         return {"model_name": model_name, "message": "LeCNN all the images"}
-
-#     return {"model_name": model_name, "message": "Have some residuals"}
-
-# # ----------------- Query Parameters -----------------
-
-# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-# class FilterParams(BaseModel):
-#     limit: int = Field(100, gt=0, le=100)
-#     offset: int = Field(0, ge=0)
-#     order_by: Literal["created_at", "updated_at"] = "created_at"
-#     tags: list[str] = []
-
-# @app.get("/items/", tags=["Items"], summary="Get all items")
-# #async def read_item(skip: int = 0, limit: int = 10):
-# async def read_items(filter_query: Annotated[FilterParams, Query()]):
-#     # Slicing is a way to extract a portion of a list, tuple, string, or any other sequence type. 
-#     # sequence[start:stop:step]
-#     # start: The index where the slice starts (inclusive).
-#     # stop: The index where the slice ends (exclusive).
-#     # step: The interval between each index (optional).
-#     #return fake_items_db[skip : skip + limit]
-#     return filter_query
-
-# @app.get("/items/{item_id}", tags=["Items"], summary="Get a specific item")
-# async def read_item(item_id: str, q: str | None = None, short: bool = False):
-#     item = {"item_id": item_id}
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-
-# @app.get("/users/{user_id}/items/{item_id}", tags=["Items"], summary="Get a specific item for a user")
-# async def read_user_item(
-#     user_id: int, item_id: str, q: str | None = None, short: bool = False
-# ):
-#     item = {"item_id": item_id, "owner_id": user_id}
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
-
-# # ----------------- Request Body -----------------
-# class Item(BaseModel):
-#     name: str
-#     description: str | None = None
-#     price: float
-#     tax: float | None = None
-#     model_config = {
-#         "json_schema_extra": {
-#             "examples": [
-#                 {
-#                     "name": "Foo",
-#                     "description": "A very nice Item",
-#                     "price": 35.4,
-#                     "tax": 3.2,
-#                 }
-#             ]
-#         }
-#     }
-
-# class User(BaseModel):
-#     username: str = Field(examples=['joanmedia'])
-#     full_name: str | None = Field(default=None, examples=['Joan Esquivel'])
-# @app.post("/items/", tags=["Items"], summary="Create an item", status_code=status.HTTP_201_CREATED)
-# async def create_item(item: Item, user: User):
-#     results = {"item": item, "user": user}
-#     return results
-
-# @app.put("/items/{item_id}", tags=["Items"], summary="Update an item")
-# async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)], q: Annotated[str | None, Query(title="Test", description="Query string for the items to search in the database that have a good match", min_length=3,max_length=50,pattern="^fixedquery$",
-#             deprecated=True,)] = None):
-#     result = {"item_id": item_id, **item.dict()}
-#     if q:
-#         result.update({"q": q})
-#     return result
-
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
